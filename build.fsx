af8c4049a535dcee448e0103b33735875bc24dea
Chet Husk <chet_husk@volusion.com>
2015-07-15 17:41:46 -0500

    change ownership

--- build.fsx
+++ build.fsx
@@ -51,14 +51,14 @@ let testAssemblies = "tests/**/bin/Release/*Tests*.dll"
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "dirkelfman" 
+let gitOwner = "baronfel" 
 let gitHome = "https://github.com/" + gitOwner
 
 // The name of the project on GitHub
 let gitName = "MongoMigrator"
 
 // The url for the raw files hosted
-let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/dirkelfman"
+let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/baronfel"
 
 // --------------------------------------------------------------------------------------
 // END TODO: The rest of the file includes standard build steps
